7f2fc9ac9ed011c4a03dad2fcfcf9b77e6dbf5c0
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2022-01-17 22:17:41 +0100

    Initial Commit

---
+++ AppleMusic-AddRadioScreen/AppleMusic_AddRadioScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_AddRadioScreenApp.swift
+//  AppleMusic-AddRadioScreen
+//
+//  Created by Wizzard Sklyarov on 2022. 01. 17..
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_AddRadioScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusic-AddRadioScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMusic-AddRadioScreen
+//
+//  Created by Wizzard Sklyarov on 2022. 01. 17..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
